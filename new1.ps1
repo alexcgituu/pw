73e01f32ad944990bddf94c0bdac1df7815d1afe
alexcgituu <uu6425b@gmail.com>
2024-01-19 10:51:15 +0800

    1

--- new1.ps1
+++ new1.ps1
@@ -1,2 +1,4 @@
 New-LocalUser -Name "TestUser" -Password (ConvertTo-SecureString -AsPlainText "N0viru$123123" -Force) -FullName "Full Name" -Description "User Description" -AccountNeverExpires
 123
+456
+dd
